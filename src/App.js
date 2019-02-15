6b8b3b602127eada81e36241feabcbcf4ce8aa94
Blesswin Samuel <blesswinsamuel@gmail.com>
2019-02-16 04:57:57 +0530

    add reach-router for testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link, Router } from '@reach/router'
 import NestedForm from './NestedForm'
+import SimpleForm from './SimpleForm'
 
 const App = () => {
-  return <NestedForm/>
+  return (
+    <div>
+      <header className="navbar">
+        <nav className="navbar-section">
+          <Link to="/simple" className="btn btn-link">Simple</Link>
+          <Link to="/nested" className="btn btn-link">Nested</Link>
+        </nav>
+      </header>
+
+      <Router>
+        <SimpleForm path="/simple"/>
+        <NestedForm path="/nested"/>
+      </Router>
+    </div>
+  )
 }
 
 export default App
