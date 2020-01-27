9d43e9f23f7ecd99e590432bdaa37f8c7183a5f5
Blesswin Samuel <blesswinsamuel@gmail.com>
2020-01-28 01:26:05 +0530

    bump deps and fix stuff

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import commonjs from '@rollup/plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import strip from 'rollup-plugin-strip'
+import resolve from '@rollup/plugin-node-resolve'
+import url from '@rollup/plugin-url'
+import strip from '@rollup/plugin-strip'
 import svgr from '@svgr/rollup'
 
 import pkg from './package.json'
@@ -35,10 +35,7 @@ export default {
     url({ exclude: ['**/*.svg'] }),
     svgr(),
     resolve(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      clean: true,
-    }),
+    typescript(),
     production && strip({ include: '**/*.ts' }),
     commonjs(),
   ].filter(x => x),
